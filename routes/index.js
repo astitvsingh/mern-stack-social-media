ec734a959ba137d80cb1b8d4eda93f1859dc94de
Astitv Singh <98196550+astitvsingh@users.noreply.github.com>
2023-01-12 10:14:33 +0530

    update

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const authRouter = require('./auth');
+const usersRouter = require('./users');
+
+/* GET home page.
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+*/
+
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
+
+module.exports = router;
